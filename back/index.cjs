324d76605dcb5cc7a267523ea8623155f95ce711
Thaylla <167567044+TataFaria@users.noreply.github.com>
2024-10-09 16:03:12 -0300

    iniciando

---
+++ back/index.cjs
@@ -0,0 +1,48 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const livroModel = require('./src/model/livro.cjs')
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 5173;
+
+app.get('/livros', async (req, res) => {
+  const livros = await livroModel.find({})
+  return res.status(200).json(livros)
+})
+
+app.post('/livros/cadastro', async (req, res) => {
+  const response = await livroModel.create({
+    id: req.body.id,
+    titulo: req.body.titulo,
+    num_paginas: req.body.num_paginas,
+    isbn: req.body.isbn,
+    editora: req.body.editora
+  })
+  return res.status(200).json({
+    data: response
+  })
+})
+
+app.get('/livros/edicao/:id', async (req, res) => {
+  const livro = await livroModel.findOne({ id: req.params.id})
+  return res.status(200).json(livro)
+})
+
+app.put('/livros/edicao/:id', async (req, res) => {
+  const livro = await livroModel.updateOne({ id: req.params.id }, req.body)
+  return res.status(200).json(livro)
+})
+
+app.delete('/livros/:id', async (req, res) => {
+  const livro = await livroModel.deleteOne({ id: req.params.id })
+  return res.status(200).json(livro)
+})
+
+app.listen(5173, () => {
+    console.log('Servidor operacional!')
+})
